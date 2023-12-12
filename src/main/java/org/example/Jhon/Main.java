da43c59e16e89ffb7f994c1260c159ff259c7d82
jhon <pabongamas@gmail.com>
2023-12-12 10:35:09 -0500

    cambio

--- src/main/java/org/example/Jhon/Main.java
+++ src/main/java/org/example/Jhon/Main.java
@@ -2,8 +2,6 @@ package org.example.Jhon;
 
 import java.sql.Connection;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
       Conexion conexion=new Conexion();
